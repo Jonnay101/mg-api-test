f9dd1b218e9f0c3cac6af3368cde5d644c8444f4
Jonnay <john.hughes101@gmail.com>
2018-05-08 20:22:53 +0100

    trying to fix heroku deployment... removed app.use all

--- index.js
+++ index.js
@@ -16,9 +16,6 @@ mongoose.Promise = global.Promise;
 // parse the incoming data
 app.use(bodyParser.json());
 
-
-
-
 // create the routes for comp presets
 app.use('/api', fxRoutes);
 
@@ -31,10 +28,10 @@ app.use(function (err, req, res, next) {
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+    // const path = require('path');
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    // });
 }
 
 const PORT = process.env.PORT || 5000;
