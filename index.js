86bdca0f01392640517aaf893f0a0006203400cc
Jonnay <john.hughes101@gmail.com>
2018-05-08 18:53:17 +0100

    trial fix2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { mongoURI } = require('./config/keys');
 const app = express();
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static('api/user1234/client/build'));
 }
 
 // connect to mongodb
